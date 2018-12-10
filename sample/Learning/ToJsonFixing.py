83a7f862ae3f14ed5d26a33aec3262fd78739083
NathanPG <nqpenguin@gmail.com>
2018-12-09 21:26:04 -0500

    Create ToJsonFixing.py
    Fixing corresponding json file

---
+++ sample/Learning/ToJsonFixing.py
@@ -0,0 +1,8 @@
+import urllib
+from urllib import request
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+from datetime import  datetime
+import numpy as np
+
+#Currently working on this to fix json file bugs
